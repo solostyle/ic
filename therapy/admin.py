6b6ead4ae7c46771fabc041dd16323ff146827f9
solostyle <spam@solostyle.net>
2012-07-15 14:34:38 -0500

    admin for exercises, jointactions, and muscles

---
+++ therapy/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from therapy.models import Exercise, JointAction, Muscle
+
+admin.site.register(Exercise)
+admin.site.register(JointAction)
+admin.site.register(Muscle)
