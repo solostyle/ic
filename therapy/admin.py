fbea17eadd58e50f209a3215ce142fa5e10a4fa9
solostyle <spam@solostyle.net>
2012-07-15 14:40:43 -0500

    changed order of fields in admin for exercises

--- therapy/admin.py
+++ therapy/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from therapy.models import Exercise, JointAction, Muscle
 
-admin.site.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'name', 'description', 'joint_action']
+
+admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(JointAction)
 admin.site.register(Muscle)
