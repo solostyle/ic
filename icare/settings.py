632b4933596bd901c379136faa474810f84f71f4
solostyle <spam@solostyle.net>
2012-08-19 16:11:55 -0500

    nothing

--- icare/settings.py
+++ icare/settings.py
@@ -20,6 +20,8 @@ DATABASES = {
     }
 }
 
+#DATABASE_URL = mysql://solostyle:qas??wed@mysql.solostyle.net/icaredb
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
