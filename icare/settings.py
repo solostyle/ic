e62383e3f6cf24b2c8dc9d1d9b79772154bed344
solostyle <spam@solostyle.net>
2012-08-21 13:54:08 -0500

    os-based path for template dir

--- icare/settings.py
+++ icare/settings.py
@@ -1,4 +1,7 @@
 # Django settings for icare project.
+import os
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -112,7 +115,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/a/www/ic/icare/templates',
+#	'/home/a/www/ic/icare/templates',
+	PROJECT_PATH + '/templates/'
 )
 
 INSTALLED_APPS = (
