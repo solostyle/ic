2bd3956404b552104e292db060f54fdd42cb55ac
solostyle <spam@solostyle.net>
2012-08-21 13:47:03 -0500

    templates and views

--- therapy/views.py
+++ therapy/views.py
@@ -1,7 +1,14 @@
+from django.template import Context, loader
+from therapy.models import Exercise, JointAction, PoseCategory, Muscle
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Therapy index.")
+	latest_exercise_list = Exercise.objects.all().order_by('-pub_date')[:5]
+	t = loader.get_template('exercises/index.html')
+	c = Context({
+		'latest_exercise_list': latest_exercise_list,
+	})
+	return HttpResponse(t.render(c))
 
 def detail(request, exercise_id):
 	return HttpResponse("Exercise %s." % exercise_id)
