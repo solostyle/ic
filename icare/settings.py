bdf5192f97d7e3ac73972f2a7e0bfaaa3f98b25c
solostyle <spam@solostyle.net>
2012-08-21 13:56:22 -0500

    os-based paths for other settings

--- icare/settings.py
+++ icare/settings.py
@@ -53,7 +53,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/a/www/ic/icare/media/'
+MEDIA_ROOT = PROJECT_PATH + '/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +64,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/a/www/ic/icare/media/static/'
+STATIC_ROOT = PROJECT_PATH + '/media/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -75,7 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # '/home/a/www/ic/icare/static',
+    PROJECT_PATH + '/static',
 )
 
 # List of finder classes that know how to find static files in
