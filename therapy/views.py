863846f328402fb3f11f5f24f3e289cca53c4192
solostyle <spam@solostyle.net>
2012-08-21 14:43:38 -0500

    detail view and 404 and 500 error pages

--- therapy/views.py
+++ therapy/views.py
@@ -1,17 +1,14 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
 from therapy.models import Exercise, JointAction, PoseCategory, Muscle
-from django.http import HttpResponse
+from django.http import Http404
 
 def index(request):
 	latest_exercise_list = Exercise.objects.all().order_by('-pub_date')[:5]
-	t = loader.get_template('exercises/index.html')
-	c = Context({
-		'latest_exercise_list': latest_exercise_list,
-	})
-	return HttpResponse(t.render(c))
+	return render_to_response('exercises/index.html', {'latest_exercise_list': latest_exercise_list})
 
 def detail(request, exercise_id):
-	return HttpResponse("Exercise %s." % exercise_id)
+	e = get_object_or_404(Exercise, pk=exercise_id)
+	return render_to_response('exercises/detail.html', {'exercise': e})
 
 def joints(request, exercise_id):
 	return HttpResponse("Joints of Exercise %s." % exercise_id)
